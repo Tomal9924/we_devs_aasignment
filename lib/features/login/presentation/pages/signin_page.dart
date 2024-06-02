0e08eadbac73b51c621fe096a0ad3b1a5c7194d3
Tanvir Ahmed <tomalahmed350@gmail.com>
2024-06-02 19:30:07 +0600

    signup text modified

--- lib/features/login/presentation/pages/signin_page.dart
+++ lib/features/login/presentation/pages/signin_page.dart
@@ -25,8 +25,10 @@ class _SignInPageState extends State<SignInPage> {
 
   @override
   void initState() {
-    emailValidator.initialize(controller: _emailController, type: FormType.email);
-    passwordValidator.initialize(controller: _passwordController, type: FormType.password);
+    emailValidator.initialize(
+        controller: _emailController, type: FormType.email);
+    passwordValidator.initialize(
+        controller: _passwordController, type: FormType.password);
 
     super.initState();
   }
@@ -43,11 +45,14 @@ class _SignInPageState extends State<SignInPage> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Image.asset("icons/logo.png", fit: BoxFit.contain, width: 144, height: 144),
+                Image.asset("icons/logo.png",
+                    fit: BoxFit.contain, width: 144, height: 144),
                 const SizedBox(height: 48),
                 Text(
                   "Sign in",
-                  style: context.textStyle17Medium(color: theme.textPrimary).copyWith(height: 1.2, fontWeight: FontWeight.bold),
+                  style: context
+                      .textStyle17Medium(color: theme.textPrimary)
+                      .copyWith(height: 1.2, fontWeight: FontWeight.bold),
                 ),
                 const SizedBox(height: 48),
                 Column(
@@ -58,8 +63,13 @@ class _SignInPageState extends State<SignInPage> {
                       controller: _emailController,
                       keyboardType: TextInputType.emailAddress,
                       style: TextStyles.body(
-                          context: context, color: emailValidator.isValid ? theme.textPrimary : theme.errorColor),
-                      cursorColor: emailValidator.isValid ? theme.successColor : theme.errorColor,
+                          context: context,
+                          color: emailValidator.isValid
+                              ? theme.textPrimary
+                              : theme.errorColor),
+                      cursorColor: emailValidator.isValid
+                          ? theme.successColor
+                          : theme.errorColor,
                       textInputAction: TextInputAction.next,
                       onChanged: (value) {
                         if (!emailValidator.isValid) {
@@ -71,15 +81,23 @@ class _SignInPageState extends State<SignInPage> {
                       decoration: InputDecoration(
                         fillColor: theme.white,
                         filled: true,
-                        border: OutlineInputBorder(borderSide: BorderSide.none, borderRadius: BorderRadius.circular(16)),
-                        prefixIcon: Icon(Icons.person, color: emailValidator.isValid ? theme.textSecondary : theme.errorColor),
+                        border: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                            borderRadius: BorderRadius.circular(16)),
+                        prefixIcon: Icon(Icons.person,
+                            color: emailValidator.isValid
+                                ? theme.textSecondary
+                                : theme.errorColor),
                         contentPadding: const EdgeInsets.all(16),
                         hintText: "email",
                         hintStyle: TextStyles.body(
                             context: context,
-                            color: emailValidator.isValid ? theme.textSecondary : theme.errorColor.withOpacity(.25)),
+                            color: emailValidator.isValid
+                                ? theme.textSecondary
+                                : theme.errorColor.withOpacity(.25)),
                         helperText: emailValidator.validationMessage,
-                        helperStyle: TextStyles.caption(context: context, color: theme.errorColor),
+                        helperStyle: TextStyles.caption(
+                            context: context, color: theme.errorColor),
                       ),
                     ),
                     const SizedBox(height: 16),
@@ -88,8 +106,13 @@ class _SignInPageState extends State<SignInPage> {
                       controller: _passwordController,
                       keyboardType: TextInputType.text,
                       style: TextStyles.body(
-                          context: context, color: passwordValidator.isValid ? theme.textPrimary : theme.errorColor),
-                      cursorColor: passwordValidator.isValid ? theme.successColor : theme.errorColor,
+                          context: context,
+                          color: passwordValidator.isValid
+                              ? theme.textPrimary
+                              : theme.errorColor),
+                      cursorColor: passwordValidator.isValid
+                          ? theme.successColor
+                          : theme.errorColor,
                       textInputAction: TextInputAction.done,
                       onChanged: (value) {
                         setState(() {
@@ -99,22 +122,34 @@ class _SignInPageState extends State<SignInPage> {
                       decoration: InputDecoration(
                         fillColor: theme.white,
                         filled: true,
-                        border: OutlineInputBorder(borderSide: BorderSide.none, borderRadius: BorderRadius.circular(16)),
+                        border: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                            borderRadius: BorderRadius.circular(16)),
                         prefixIcon: Icon(Icons.lock_outline_rounded,
-                            color: passwordValidator.isValid ? theme.textSecondary : theme.errorColor),
+                            color: passwordValidator.isValid
+                                ? theme.textSecondary
+                                : theme.errorColor),
                         contentPadding: const EdgeInsets.all(16),
                         hintText: "password",
                         hintStyle: TextStyles.body(
                             context: context,
-                            color: passwordValidator.isValid ? theme.textSecondary : theme.errorColor.withOpacity(.25)),
+                            color: passwordValidator.isValid
+                                ? theme.textSecondary
+                                : theme.errorColor.withOpacity(.25)),
                         helperText: passwordValidator.validationMessage,
-                        helperStyle: TextStyles.caption(context: context, color: theme.errorColor),
+                        helperStyle: TextStyles.caption(
+                            context: context, color: theme.errorColor),
                         suffixIcon: IconButton(
                           padding: EdgeInsets.zero,
                           highlightColor: Colors.transparent,
                           splashColor: Colors.transparent,
-                          icon: Icon(passwordValidator.isObscure ? Icons.visibility : Icons.visibility_off,
-                              color: passwordValidator.isValid ? theme.textSecondary : theme.errorColor),
+                          icon: Icon(
+                              passwordValidator.isObscure
+                                  ? Icons.visibility
+                                  : Icons.visibility_off,
+                              color: passwordValidator.isValid
+                                  ? theme.textSecondary
+                                  : theme.errorColor),
                           onPressed: () {
                             setState(() {
                               passwordValidator.toggleObscure();
@@ -133,8 +168,8 @@ class _SignInPageState extends State<SignInPage> {
                   height: 61.h,
                   child: BlocConsumer<LoginBloc, LoginState>(
                     listener: (context, state) {
-                      if(state is LoginSuccess) {
-                      final user=state.user;
+                      if (state is LoginSuccess) {
+                        final user = state.user;
                         BlocProvider.of<UserBloc>(context).add(
                           SaveUser(user: user),
                         );
@@ -148,7 +183,10 @@ class _SignInPageState extends State<SignInPage> {
                             backgroundColor: theme.buttonColor,
                           ),
                           child: Text('Signing in...',
-                              style: context.textStyle17Medium(color: theme.backgroundColor).copyWith(height: 1.2)),
+                              style: context
+                                  .textStyle17Medium(
+                                      color: theme.backgroundColor)
+                                  .copyWith(height: 1.2)),
                         );
                       } else if (state is LoginError) {
                         return ElevatedButton(
@@ -157,7 +195,8 @@ class _SignInPageState extends State<SignInPage> {
                               emailValidator.validate();
                               passwordValidator.validate();
                             });
-                            if (emailValidator.isValid && passwordValidator.isValid) {
+                            if (emailValidator.isValid &&
+                                passwordValidator.isValid) {
                               context.read<LoginBloc>().add(
                                     Login(
                                       email: _emailController.text,
@@ -170,7 +209,10 @@ class _SignInPageState extends State<SignInPage> {
                             backgroundColor: theme.buttonColor,
                           ),
                           child: Text('Try again!',
-                              style: context.textStyle17Medium(color: theme.backgroundColor).copyWith(height: 1.2)),
+                              style: context
+                                  .textStyle17Medium(
+                                      color: theme.backgroundColor)
+                                  .copyWith(height: 1.2)),
                         );
                       } else if (state is LoginSuccess) {
                         return const ElevatedButton(
@@ -184,7 +226,8 @@ class _SignInPageState extends State<SignInPage> {
                               emailValidator.validate();
                               passwordValidator.validate();
                             });
-                            if (emailValidator.isValid && passwordValidator.isValid) {
+                            if (emailValidator.isValid &&
+                                passwordValidator.isValid) {
                               context.read<LoginBloc>().add(
                                     Login(
                                       email: _emailController.text,
@@ -197,7 +240,10 @@ class _SignInPageState extends State<SignInPage> {
                             backgroundColor: theme.buttonColor,
                           ),
                           child: Text('Sign In',
-                              style: context.textStyle17Medium(color: theme.backgroundColor).copyWith(height: 1.2)),
+                              style: context
+                                  .textStyle17Medium(
+                                      color: theme.backgroundColor)
+                                  .copyWith(height: 1.2)),
                         );
                       }
                     },
@@ -240,12 +286,16 @@ class _SignInPageState extends State<SignInPage> {
                   text: TextSpan(
                     children: [
                       TextSpan(
-                        text: 'Already have an account?   ',
-                        style: context.textStyle17Medium(color: theme.textSecondary).copyWith(height: 1.2),
+                        text: "Don't have an account?  ",
+                        style: context
+                            .textStyle17Medium(color: theme.textSecondary)
+                            .copyWith(height: 1.2),
                       ),
                       TextSpan(
-                        text: 'Login',
-                        style: context.textStyle17Medium(color: theme.blue).copyWith(height: 1.2, fontWeight: FontWeight.bold),
+                        text: 'Sign Up',
+                        style: context
+                            .textStyle17Medium(color: theme.blue)
+                            .copyWith(height: 1.2, fontWeight: FontWeight.bold),
                       )
                     ],
                   ),
